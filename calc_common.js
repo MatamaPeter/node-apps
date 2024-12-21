513c09443fb82d4eaff8675efe40afd147a432fe
MatamaPeter <peter24matama@gmail.com>
2024-12-21 11:42:20 +0300

    `Added interactive equation solver using readline module`

--- calc_common.js
+++ calc_common.js
@@ -1,2 +1,26 @@
 const readline = require('readline');
-const rl = readline.createInterface
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function solve() {
+
+    rl.question('Enter you equation: ', (input) => {
+        const equation = input.trim();
+        if (equation.toLowerCase() === 'quit') {
+            rl.close();
+        } else {
+            try {
+                const answer = eval(equation);
+                console.log(`The answer is: ${answer}`);
+            } catch (error) {
+                console.log('Invalid input');
+            
+            }
+            solve();
+        }
+    })
+}
+solve();
