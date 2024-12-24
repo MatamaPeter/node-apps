8a8ab361362a084b76da6edbe6a0beea107afb4d
MatamaPeter <peter24matama@gmail.com>
2024-12-24 11:31:13 +0300

    added a  node js server

--- Event emitter/logEvents.js
+++ Event emitter/logEvents.js
@@ -6,7 +6,7 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 
-const logEvents = async (message,logName) => {
+const logEvents = async (message) => {
     
     const dateTime = `${format(new Date(), 'yyyy/MM/dd\tHH:mm:ss')}`
     const logItem = `${dateTime} \t ${uuid()} \t ${message}\n`
@@ -15,7 +15,7 @@ const logEvents = async (message,logName) => {
         if (!fs.existsSync('./logs')) {
             await fsPromises.mkdir('./logs')
         } else {
-            await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+            await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLogs.txt'), logItem);
         }
     }
     catch(err) {
